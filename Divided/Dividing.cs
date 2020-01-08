79a7716b4845c7716991e412ef9541fdbe20982b
abigail <abigailnankamba@gmail.com>
2020-01-08 16:51:33 +0200

    Feature: Added a Division Interface

---
+++ Divided/Dividing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Operation;
+namespace Divided
+
+{
+    public interface IDiving
+    {
+        public dynamic Squared(dynamic a, dynamic b);
+    }
+    public class Dividing: IDiving
+    {
+        public dynamic result;
+        public dynamic Squared(dynamic a, dynamic b)
+        {
+            result = Division.Divsion(a,b);
+            return result;
+        }
+    }
+}
